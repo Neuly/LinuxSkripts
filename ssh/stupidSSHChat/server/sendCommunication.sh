1777a9a2d7375c8ab14e6b5b09363aea1c185dca
Neuly <neulingerph@stud.sbg.ac.at>
2018-12-14 10:02:10 +0100

    Committed everything

---
+++ ssh/stupidSSHChat/server/sendCommunication.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# append to communication file
+
+echo -e "\e[90mServer\e[0m: $1" >> communication
---
+++ ssh/stupidSSHChat/server/tailFile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp communication communication_backup
+
+tail --lines=50 communication_backup > communication
+
+
